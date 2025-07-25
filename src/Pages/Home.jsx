23dc978c1d11d0bb2cc0aed7ecb5fafb364fd0e8
Abhinav Vishen <av.abhinavvishen@gmail.com>
2025-07-26 02:43:18 +0530

    Added Minimum height CSS in Home.jsx and in AllPost.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,27 +23,29 @@ const Home = () => {
     if (posts.length === 0) {
         return (
             <div className="w-full py-8 mt-4 text-center">
-            <Container>
-                <div className="flex flex-wrap">
-                    <div className="p-2 w-full h-[65vh] flex justify-center items-center border rounded-2xl">
-                        <h1 className="text-2xl font-bold hover:text-gray-500">
-                            No Posts Found!
-                        </h1>
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full h-[65vh] flex justify-center items-center border rounded-2xl">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                No Posts Found!
+                            </h1>
+                        </div>
                     </div>
-                </div>
-            </Container>
-        </div>
+                </Container>
+            </div>
         )
     }
     return (
         <div className='w-full py-8'>
             <Container>
-                <div className='flex flex-wrap'>
-                    {posts.map((post) => (
-                        <div key={post.$id} className='p-2 w-1/4'>
-                            <PostCard {...post} />
-                        </div>
-                    ))}
+                <div className="w-full min-h-[70vh]">
+                    <div className='flex flex-wrap'>
+                        {posts.map((post) => (
+                            <div key={post.$id} className='p-2 w-1/4'>
+                                <PostCard {...post} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </Container>
 
